ef0ae78a2180532109b48ffb8fa15372f03f24cd
jsomasekhar123 <jsoma21498@gmail.com>
2025-03-25 16:35:06 +0530

    even number

---
+++ javademo/src/javaprogram/EvenNumber.java
@@ -0,0 +1,12 @@
+package javaprogram;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        System.out.println("Even number:");
+        for (int i = 0; i <=10; i++) {
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
